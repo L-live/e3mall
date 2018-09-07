5d6746ad04124b6019a98a5734a809eaffc07620
lxl <xilu_liu@163.com>
2018-09-07 15:40:38 +0800

    SOA架构

--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -1,10 +1,17 @@
 package cn.e3mall.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import cn.e3mall.common.pojo.DataGridResult;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.service.ItemService;
 
 @Service
@@ -17,6 +24,19 @@ public class ItemServiceImpl implements ItemService {
 
 		TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
 		return tbItem;
+	}
+	@Override
+	public DataGridResult getItemList(int page, int rows) {
+
+		PageHelper.startPage(page, rows);
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = itemMapper.selectByExample(example);
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		long total = pageInfo.getTotal();
+		DataGridResult result = new DataGridResult();
+		result.setTotal(total);
+		result.setRows(list);
+		return result;
 	}
 
 }
